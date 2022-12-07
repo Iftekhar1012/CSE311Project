970203f55b1d403a9cb2b35dfff07804770b88fd
Tanvir Chowdhury <117327874+Tachow@users.noreply.github.com>
2022-12-07 22:55:49 +0600

    provider dashboard php file added

---
+++ provider_dashboard.php
@@ -0,0 +1,27 @@
+<?php
+//include auth_session.php file on all user panel pages
+include("auth_session.php");
+include("db.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Service provider dashboard</title>
+    <link rel="stylesheet" href="style.css" /> <!--stylesheet not added yet-->
+</head>
+<body>
+    <div class="form">
+        <?php 
+        $sql = "SELECT username FROM `providers`";
+        $result = mysqli_query($con,$sql);
+        $row = mysqli_fetch_assoc($result);
+        
+        
+        ?>
+        <p>Hey, <?php echo $_SESSION['username']; ?>!</p>
+        <p>You are now on service provider dashboard page.</p>
+        <p><a href="logout.php">Logout</a></p>
+    </div>
+</body>
+</html>
