2e9954be25d646d996d926a3f393dae4fb37cd00
Iftekhar Chowdhury <52180135+Iftekhar1012@users.noreply.github.com>
2022-12-11 21:17:22 +0600

    Add files via upload

---
+++ lp.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>LP</title>
+
+<style> 
+section{
+width: 100%;
+height: 100vh;
+color: #fff;
+background: linear-gradient(-45deg, #EE7752, #E73C7e, #23A6D5);
+background-size: 400% 400%;
+position: relative;
+
+}
+
+
+header .navigation a{
+color:#000;
+text-decoration: none;
+font-weight: 500;
+letter-spacing:1px;
+padding: 2px 15px;
+border-radius: 20px;
+}
+
+
+
+</style>
+</head>
+
+<body>
+ <section>
+  <header>
+    <h2> <a href='#' class = "logo"></a><h2>
+    <div class ="navigation">
+    <a href="login.php">Login</a>
+    <a href='#'>About us</a>
+    <a href='#'>Contact</a>
+    </div>
+    <header>
+    
+    <div class = "content">
+    <div class = "info">
+    <h1>ANYTHING YOU NEED,<br><span>JUST ASK</span></h1>
+
+    <h1><a href="registration.php">SIGN UP</a></h1>
+    
+    </div>
+    </div>
+    </section>
+    
+    </body>
+    </html>
+    
+
+
+ 
+
+
+
+
+
+
+</html>
